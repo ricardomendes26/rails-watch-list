939e22012507bc5fb9446745e6aab835b10586de
ricardomendes <ricardogaditmendes26@gmail.com>
2022-11-18 12:48:54 +0000

    new db

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@ Movie.destroy_all
 puts "Database cleaned"
 
 url = "http://tmdb.lewagon.com/movie/top_rated"
-10.times do |i|
+20.times do |i|
   puts "Importing movies from page #{i + 1}"
   movies = JSON.parse(URI.open("#{url}?page=#{i + 1}").read)['results']
   movies.each do |movie|
